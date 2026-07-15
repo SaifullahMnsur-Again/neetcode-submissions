25aa68001b59c1bc8614f756520813a883c72bb4
Sayfullah Monsur Sujan Sarkar <210016429+SaifullahMnsur-Again@users.noreply.github.com>
2026-07-15 13:30:53 +0600

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& p, vector<int>& s) {
+        vector<pair<int, int>> vp;
+
+        for(int i = 0; i < p.size(); i++) {
+            vp.push_back({p[i], s[i]});
+        }
+        sort(vp.rbegin(), vp.rend());
+
+        set<double> st;
+        for(auto i : vp) {
+            double need = (double)(target - i.first) / i.second;
+            if(!st.empty()) {
+                if(*st.rbegin() >= need) {
+                    continue;
+                }
+            }
+            
+            st.insert(need);
+        }
+        
+        return st.size();
+    }
+};
