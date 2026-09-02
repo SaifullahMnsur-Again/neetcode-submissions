6b0f3fb6ec13add8aa1934ea83469cc202967f3c
Sayfullah Monsur Sujan Sarkar <210016429+SaifullahMnsur-Again@users.noreply.github.com>
2026-09-02 20:36:19 +0600

    Add: interval-list-intersections - submission-1

---
+++ Data Structures & Algorithms/interval-list-intersections/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& a, vector<vector<int>>& b) { 
+        int n = a.size();
+        int m = b.size();
+
+        vector<vector<int>> ans;
+        int i = 0, j = 0;
+
+        while(i < a.size() and j < b.size()) {
+            int start = max(a[i][0], b[j][0]);
+            int end = min(a[i][1], b[j][1]);
+            if(start <= end) {
+                ans.push_back({start, end});
+            }
+
+            if(a[i][1] < b[j][1])
+                i++;
+            else
+                j++;
+        }
+
+        return ans;
+    }
+};
