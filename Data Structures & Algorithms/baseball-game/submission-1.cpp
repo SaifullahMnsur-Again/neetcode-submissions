916453c94b254c90954c098958a0205df7bc4ad5
Sayfullah Monsur Sujan Sarkar <210016429+SaifullahMnsur-Again@users.noreply.github.com>
2026-09-08 20:08:35 +0600

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> stc;
+        for(auto op : operations) {
+            if(op == "+") {
+                int a = stc.top(); stc.pop();
+                int s = a + stc.top();
+                stc.push(a);
+                stc.push(s);
+            } else if(op == "C") {
+                stc.pop();
+            } else if(op == "D") {
+                int p = 2 * stc.top();
+                stc.push(p);
+            } else {
+                stc.push(stoi(op));
+            }
+        }
+        int sum = 0;
+        while(!stc.empty()) {
+            sum += stc.top();
+            stc.pop();
+        }
+        return sum; 
+    }
+};
