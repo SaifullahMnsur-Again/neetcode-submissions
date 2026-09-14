7146a584b1501267f216389c218760aa92658777
Sayfullah Monsur Sujan Sarkar <210016429+SaifullahMnsur-Again@users.noreply.github.com>
2026-09-15 02:11:51 +0600

    Add: sql-sales-person - submission-1

---
+++ SQL/sql-sales-person/submission-1.sql
@@ -0,0 +1,24 @@
+-- Write your query below
+WITH No_CRIMSON AS (
+    SELECT
+        sales_id
+    FROM
+        orders o
+    JOIN    
+        company c
+        ON
+            o.com_id = c.com_id
+    WHERE
+        c.name = 'CRIMSON'
+)
+SELECT
+    sp.name
+FROM
+    sales_person sp
+WHERE
+    sp.sales_id NOT IN (
+        SELECT
+            sales_id
+        FROM
+            No_CRIMSON
+    );
