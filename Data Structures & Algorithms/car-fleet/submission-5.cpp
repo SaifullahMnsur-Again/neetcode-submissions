dcf58263ec0d8f2ff244c07b32d6793f373e84ab
Sayfullah Monsur Sujan Sarkar <210016429+SaifullahMnsur-Again@users.noreply.github.com>
2026-09-17 23:16:02 +0600

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    double getTime(int target, int position, int speed) {
+        return (double)(target - position) / speed;
+    }
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> ps;
+        for(int i = 0; i < position.size(); i++) {
+            ps.push_back({position[i], speed[i]});
+        }
+        sort(ps.begin(), ps.end());
+        double minTime = getTime(target, ps.back().first, ps.back().second);
+        cerr << minTime << endl;
+        int ans = 1;
+        for(int i = position.size() - 2; i >= 0; i--) {
+            double currTime = getTime(target, ps[i].first, ps[i].second);
+            cerr << currTime << endl;
+            if(currTime - minTime> 1e-6) {
+                ans++;
+                minTime = currTime;
+            }
+        }
+
+        return ans;
+    }
+};
