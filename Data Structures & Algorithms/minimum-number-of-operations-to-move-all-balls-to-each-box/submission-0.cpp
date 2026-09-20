6996c90a595f01d544aaf776ffd5873dc67f240f
Sayfullah Monsur Sujan Sarkar <210016429+SaifullahMnsur-Again@users.noreply.github.com>
2026-09-20 21:27:01 +0600

    Add: minimum-number-of-operations-to-move-all-balls-to-each-box - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-operations-to-move-all-balls-to-each-box/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size();
+        int l = 0, r = 0;
+        int ls = 0, rs = 0;
+        for(int i = 1; i < n; i++) {
+            if(boxes[i] == '1') {
+                r++;
+                rs += i;
+            }
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            cerr << l << " " << r << endl;
+            ans.push_back(ls + rs);
+            rs -= r;
+            if(boxes[i] == '1')
+                l++;
+            if(i < n - 1 and boxes[i + 1] == '1')
+                r--;
+            ls += l;
+        }
+        return ans;
+    }
+};
